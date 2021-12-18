391f8be417e77a87cf67274a3e7f6c219ca3cae3
add-flav1s <aflavia17_@hotmail.com>
2021-12-18 16:17:54 -0300

    configurando os modulos e add as pastas do projeto.

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+//Carregando os modulos do app
+
 const express = require('express');
+const handlebars = require('express-handlebars');
 const app = express();
+
+
+//Configurando os modulos
+// ----body-parser configure ---- //
+app.use(bodyParser.urlenconded({
+    extendend: true
+}));
+app.use(bodyParser.json());
+
+// ----handlebars configure ---- //
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// ----mongoose configure ---- //
+
+
+
+
 const port = 8082;
-
-app.get('/', (req, res) => {
-    res.send('hello world');
-})
-
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
-})
+});
